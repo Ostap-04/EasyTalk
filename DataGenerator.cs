78288a65bb4f83216f5afd2d03387018bf1d3f7e
Vorobienko Bohdan <bogdanvorobienko@gmail.com>
2023-10-17 11:44:26 +0300

    added indices

---
+++ DataGenerator.cs
@@ -0,0 +1,232 @@
+﻿using Npgsql;
+using System.Text;
+
+namespace dbSetup
+{
+    public class DataGenerator
+    {
+        private static readonly Random random = new Random();
+        static int recordsNum = 40;
+        private static string[] emailDomains = { "gmail.com", "yahoo.com", "outlook.com", "example.com" };
+
+        public static void InsertRandomData(string connection)
+        {
+            List<int> IndustryIdsList = new List<int>();
+            List<Guid> PersonIdsList = new List<Guid>();
+            List<Guid> ChatIdsList = new List<Guid>();
+            List<Guid> ProjectIdsList = new List<Guid>();
+            List<Guid> TopicIdsList = new List<Guid>();
+            List<Guid> DocumentIdsList = new List<Guid>();
+
+            using (NpgsqlConnection con = new NpgsqlConnection(connection))
+            {
+                con.Open();
+                using (NpgsqlCommand cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = con;
+                    //Insert random data into industry table
+                    for (int i = 0; i < recordsNum; i++)
+                    {
+                        cmd.CommandText = $"INSERT INTO industry(industry_name) VALUES('{GenerateRandomString(7)}')";
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    string sql = "SELECT id FROM industry;";
+                    using (NpgsqlCommand command = new NpgsqlCommand(sql, con))
+                    {
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int id = reader.GetInt32(reader.GetOrdinal("id"));
+                                IndustryIdsList.Add(id);
+                            }
+                        }
+                    }
+
+                    
+                    // Insert random data into person table
+                    for (int i = 0; i < recordsNum; i++)
+                    {
+                        string firstName = GenerateRandomString(6);
+                        string lastName = GenerateRandomString(10);
+                        string email = $"{firstName.ToLower()}.{lastName.ToLower()}@{emailDomains[random.Next(emailDomains.Length)]}";
+                        string type = random.Next(10) == 0 ? "Admin" : "User";
+                        string password = GenerateRandomString(10); // Set a default password for example purposes
+
+                        cmd.CommandText = $"INSERT INTO person(industry_id, firstname, surname, email, type, password) VALUES({IndustryIdsList[random.Next(0, IndustryIdsList.Count)]}, '{firstName}', '{lastName}', '{email}', '{type}', '{password}')";
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // Insert random data into chat table
+                    for (int i = 0; i < recordsNum; i++)
+                    {
+                        cmd.CommandText = $"INSERT INTO chat(name, created_at, updated_at, description) VALUES('{GenerateRandomString(6)}', now(), now(), 'Chat room for {GenerateRandomString(15)}')";
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    sql = "SELECT id FROM person;";
+                    using (NpgsqlCommand command = new NpgsqlCommand(sql, con))
+                    {
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Guid id = reader.GetGuid(reader.GetOrdinal("id"));
+                                PersonIdsList.Add(id);
+                            }
+                        }
+                    }
+
+                    sql = "SELECT id FROM chat;";
+                    using (NpgsqlCommand command = new NpgsqlCommand(sql, con))
+                    {
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Guid id = reader.GetGuid(reader.GetOrdinal("id"));
+                                ChatIdsList.Add(id);
+                            }
+                        }
+                    }
+
+                    // Insert random data into document table
+                    for (int i = 0; i < recordsNum; i++)
+                    {
+                        cmd.CommandText = $"INSERT INTO document(filename, file_path) VALUES('file{i}.txt', '/path/to/file{i}.txt')";
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    sql = "SELECT id FROM document;";
+                    using (NpgsqlCommand command = new NpgsqlCommand(sql, con))
+                    {
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Guid id = reader.GetGuid(reader.GetOrdinal("id"));
+                                DocumentIdsList.Add(id);
+                            }
+                        }
+                    }
+
+
+                    // Insert random data into message table
+                    for (int i = 0; i < recordsNum; i++)
+                    {
+                        cmd.CommandText = $"INSERT INTO message(created_at, updated_at, text, from_id, to_id, chat_id, attachment_id) VALUES(now(), now(), '{GenerateRandomString(20)}', '{PersonIdsList[random.Next(0, PersonIdsList.Count)]}', '{PersonIdsList[random.Next(0, PersonIdsList.Count)]}', '{ChatIdsList[random.Next(0, ChatIdsList.Count)]}', '{DocumentIdsList[random.Next(0, DocumentIdsList.Count)]}')";
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // Insert random data into person_chat table
+                    for (int i = 0; i < recordsNum; i++)
+                    {
+                        cmd.CommandText = $"INSERT INTO person_chat(person_id, chat_id) VALUES('{PersonIdsList[random.Next(0, PersonIdsList.Count)]}', '{ChatIdsList[random.Next(0, ChatIdsList.Count)]}')";
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // Insert random data into project table
+                    for (int i = 0; i < recordsNum; i++)
+                    {
+                        cmd.CommandText = $"INSERT INTO project(name, description, client_id, dev_id) VALUES('{GenerateRandomString(8)}', 'Description for {GenerateRandomString(15)}', '{PersonIdsList[random.Next(0, PersonIdsList.Count)]}', '{PersonIdsList[random.Next(0, PersonIdsList.Count)]}')";
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    sql = "SELECT id FROM project;";
+                    using (NpgsqlCommand command = new NpgsqlCommand(sql, con))
+                    {
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Guid id = reader.GetGuid(reader.GetOrdinal("id"));
+                                ProjectIdsList.Add(id);
+                            }
+                        }
+                    }
+
+                    // Insert random data into project_industry table
+                    for (int i = 0; i < recordsNum; i++)
+                    {
+                        cmd.CommandText = $"INSERT INTO project_industry(project_id, industry_id) VALUES('{ProjectIdsList[random.Next(0, ProjectIdsList.Count)]}', '{IndustryIdsList[random.Next(0, IndustryIdsList.Count)]}')";
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // Insert random data into topic table
+                    for (int i = 0; i < recordsNum; i++)
+                    {
+                        cmd.CommandText = $"INSERT INTO topic(name, project_id) VALUES('{GenerateRandomString(6)}', '{ProjectIdsList[random.Next(0, ProjectIdsList.Count)]}')";
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    sql = "SELECT id FROM topic;";
+                    using (NpgsqlCommand command = new NpgsqlCommand(sql, con))
+                    {
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Guid id = reader.GetGuid(reader.GetOrdinal("id"));
+                                TopicIdsList.Add(id);
+                            }
+                        }
+                    }
+
+                    // Insert random data into timepoint table
+                    for (int i = 0; i < recordsNum; i++)
+                    {
+                        cmd.CommandText = $"INSERT INTO timepoint(date, topic_id) VALUES(now(), '{TopicIdsList[random.Next(0, TopicIdsList.Count)]}')";
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    Console.WriteLine("----- Random data inserted successfully -----");
+                }
+            }
+        }
+
+        public static string GenerateRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            StringBuilder randomString = new StringBuilder();
+
+            Random random = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                randomString.Append(chars[random.Next(chars.Length)]);
+            }
+
+            return randomString.ToString();
+        }
+
+        public static void CreateIndices(string connectionStr)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionStr))
+            {
+                connection.Open();
+                CreateIndex("PERSON", new string[] { "nickname" }, connection);
+                CreateIndex("PERSON_CHAT", new string[] { "person_id", "chat_id" }, connection);
+                CreateIndex("CHAT", new string[] { "name" }, connection);
+                CreateIndex("MESSAGE", new string[] { "created_at" }, connection);
+                CreateIndex("DOCUMENT", new string[] { "filename" }, connection);
+                CreateIndex("INDUSTRY", new string[] { "industry_name", }, connection);
+                CreateIndex("PROJECT_INDUSTRY", new string[] { "project_id", "industry_id" }, connection);
+                CreateIndex("PROJECT", new string[] { "name" }, connection);
+                CreateIndex("TOPIC", new string[] { "name" }, connection);
+                CreateIndex("TIMEPOINT", new string[] { "date" }, connection);
+            }
+        }
+
+        private static void CreateIndex(string table, string[] columns, NpgsqlConnection connection) 
+        {
+            using (NpgsqlCommand cmd = new NpgsqlCommand()) 
+            {
+                cmd.Connection = connection;
+                string colsStr = string.Join(", ", columns);
+                string command = $"CREATE INDEX idx_{table} ON {table}({colsStr});";
+                cmd.CommandText = command;                
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,182 @@
+﻿using DotNetEnv;
+using Npgsql;
+using System.Text;
+
+namespace dbSetup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Env.Load("..\\..\\..\\.env");
+
+            var dbusername = Environment.GetEnvironmentVariable("DB_USERNAME");
+            var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+            var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            var connectionString = $"Host=localhost;Port=5432;Username={dbusername};Password={dbPassword};";
+            var connectionDBString = $"Host=localhost;Port=5432;Username={dbusername};Password={dbPassword};Database={dbName};";
+
+            try
+            {
+                CreateDB(dbName, connectionString);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                CreateTables(connectionDBString);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                DataGenerator.InsertRandomData(connectionDBString);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                DataGenerator.CreateIndices(connectionDBString);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void CreateDB(string dbName, string connectionString)
+        {
+            
+
+            var conectionString = connectionString;
+
+            using (NpgsqlConnection con = new NpgsqlConnection(conectionString))
+            {
+                con.Open();
+                var createDBStr = $"CREATE DATABASE {dbName};";
+
+                using (NpgsqlCommand cmd = new NpgsqlCommand(createDBStr, con))
+                {
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("----- DB created successsfuly -----");
+                }
+            }
+        }
+
+        
+
+        //static string GenerateRandomEmail(int length)
+        //{
+        //    return GenerateRandomString(length) + "@gmail.com";
+        //}
+
+        static void CreateTables(string conectionString)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(conectionString))
+            {
+                con.Open();
+                List<string> cmdList = new List<string>();
+
+                cmdList.Add("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";");
+
+                var createTable = @$"CREATE TABLE industry(
+                    id SERIAL PRIMARY KEY,
+                    industry_name VARCHAR(100)
+                );";
+                cmdList.Add(createTable);
+
+                createTable = @$"CREATE TABLE person(
+                    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+                    industry_id SERIAL REFERENCES industry(id),
+                    firstname VARCHAR(100),
+                    surname VARCHAR(100),
+                    nickname VARCHAR(100),
+                    email VARCHAR(100),
+                    type VARCHAR(30),
+                    password VARCHAR(100),
+                    photo BYTEA
+                );";
+                cmdList.Add(createTable);
+                
+                createTable = @$"CREATE TABLE chat(
+                    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+                    created_at TIMESTAMP WITH TIME ZONE,
+                    updated_at TIMESTAMP WITH TIME ZONE,
+                    name VARCHAR(100),
+                    description TEXT
+                );";
+                cmdList.Add(createTable);
+                
+                createTable = @$"CREATE TABLE person_chat(
+                    person_id UUID REFERENCES person(id),
+                    chat_id UUID REFERENCES chat(id)
+                );";
+                cmdList.Add(createTable);
+
+                createTable = @$"CREATE TABLE document(
+                    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+                    filename VARCHAR(100),
+                    file_path VARCHAR(150)
+                );";
+                cmdList.Add(createTable);
+
+                createTable = @$"CREATE TABLE message(
+                    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+                    created_at TIMESTAMP WITH TIME ZONE,
+                    updated_at TIMESTAMP WITH TIME ZONE,
+                    text TEXT,
+                    from_id UUID REFERENCES person(id),
+                    to_id UUID REFERENCES person(id),
+                    chat_id UUID REFERENCES chat(id),
+                    attachment_id UUID REFERENCES document(id)
+                );";
+                cmdList.Add(createTable);
+
+                createTable = @$"CREATE TABLE project(
+                    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+                    name VARCHAR(100),
+                    description TEXT,
+                    client_id UUID REFERENCES person(id),
+                    dev_id UUID REFERENCES person(id)
+                );";
+                cmdList.Add(createTable);
+                
+                createTable = @$"CREATE TABLE project_industry(
+                    project_id UUID REFERENCES project(id),
+                    industry_id SERIAL REFERENCES industry(id)
+                );";
+                cmdList.Add(createTable);
+
+                createTable = @$"CREATE TABLE topic(
+                    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+                    name VARCHAR(100),
+                    project_id UUID REFERENCES project(id)
+                );";
+                cmdList.Add(createTable);
+
+                createTable = @$"CREATE TABLE timepoint(
+                    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+                    date TIMESTAMP WITH TIME ZONE,
+                    topic_id UUID REFERENCES topic(id)
+                );";
+                cmdList.Add(createTable);
+                
+                using (NpgsqlCommand cmd = new(string.Join(" ", cmdList), con))
+                {
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine($"----- Created tables successsfuly -----");
+                }
+            }
+
+        }
+    }
+}
